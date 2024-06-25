32cd97278d9076baea3219e7ff378df1b6629188
AliciaGaona <aligaovela@gmail.com>
2024-06-25 11:38:10 -0600

    Se eliminan entornos virtuales, a que tenia problema de nombbres cuando se instalaba e instanseaa fastAPI, se agrega un nuevo entorno virtual con otro nombre y se prueba con éxito la instalacion de fastAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+#instansems esta clase
+app= FastAPI()
+
+
+#registar una ruta raiz para api http://127.0.0.1:8000
+#@ en fast api este decorador registra la funcion, cuando alguien llama a mi ruta raiz ejecuta la funcion que le sigue
+
+@app.get("/")
+def index():
+    return "Hola entre"
