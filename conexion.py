3c5a7c09eafd570f5ea0d90239c2a8b1df87c9d8
AliciaGaona <aligaovela@gmail.com>
2024-06-25 01:16:25 -0600

    se prueba conexion a base de datos exitosa, se cambiará de herramienta de python, ya que esta no permite crear apis

--- conexion.py
+++ conexion.py
@@ -7,11 +7,28 @@ usuario="sa"
 contrasena="1234"
 
 try:
-    connectionString = f'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={bd};UID={usuario};PWD={contrasena}'
+    connectionString = 'DRIVER={{ODBC Driver 18 for SQL Server}};SERVER={server};DATABASE={bd};UID={usuario};PWD={contrasena}'
     print('Conexion exitosa')
    
 except:
     print('Error de conexion bd')
 
-    pass
+pass
+
+#Consulta a base de datos
+
+#creamos un cursor para realizar la consulta
+cursor= connectionString.cursor()
+cursor.execute("Select * from [appEnews.catalogue.categories];")
+
+persona = cursor.fetchone() #primer resultado del query
+
+
+#va recorriendo para traer cada row de la tabla de bbase de datos
+while persona:
+    print(persona)
+    persona=cursor.fetchone()
+
+cursor.close()
+connectionString.close()
 
