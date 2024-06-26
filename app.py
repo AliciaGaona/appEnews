9f8d2208f0967b85053bd470d5d3f5ab2731d92a
AliciaGaona <aligaovela@gmail.com>
2024-06-26 02:51:31 -0600

    se comenta codigo

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 import conexionBD 
-try:
+
+#Se utiliza esta seccion para probar conexion, antes de pasar a contruir API y crear modelos
+try:  
+#variable que guarda la cadena de conexion que trae
   connection = conexionBD.get_connection()
   with connection.cursor() as cursor:#declaro crso para llamara sp
-     cursor.execute("exec sp_appEnewscataloguecategories_get")
-     resultset=cursor.fetchall()#todo lo que traiga
+     cursor.execute("exec sp_appEnewscataloguecategories_get")#executo sp
+     resultset=cursor.fetchall()#todo lo que traiga el sp quese ejecuto
      for row in resultset:
-        print(row)
+        print(row)#lo voy imprimiedo
 
 except Exception as ex:
     print(ex)
--- conexionBD.py
+++ conexionBD.py
@@ -5,11 +5,13 @@ import dotenv
 # cargamos las variables de entorno
 dotenv.load_dotenv()  
 
+#variables para conexión BD
 server= "ALICIAPC\SQLEXPRESS"
 bd="ExamenPruebaDev"
 usuario=os.getenv('USUARIO')
 contrasena= os.getenv('PASSWD')
 
+#funcion para realizar conexión a BD
 def get_connection():
  return pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+bd+';UID='+usuario+';PWD='+contrasena)
 #print("conexion exitosa")
