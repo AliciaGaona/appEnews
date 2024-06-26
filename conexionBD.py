3dcfe387af1122051ce0af07f64ff1c16beef8e1
AliciaGaona <aligaovela@gmail.com>
2024-06-26 02:31:07 -0600

    se agrega .gitignore

--- conexionBD.py
+++ conexionBD.py
@@ -1,9 +1,14 @@
 import pyodbc 
+import os
+import dotenv
+
+# cargamos las variables de entorno
+dotenv.load_dotenv()  
 
 server= "ALICIAPC\SQLEXPRESS"
 bd="ExamenPruebaDev"
-usuario="sa"
-contrasena="1234"
+usuario=os.getenv('USUARIO')
+contrasena= os.getenv('PASSWD')
 
 def get_connection():
  return pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+bd+';UID='+usuario+';PWD='+contrasena)
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from fastapi import FastAPI
 import conexionBD
 from routes.catalogos import catalogo
-
 #instansems esta clase
 app= FastAPI()
-
-
 #registar una ruta raiz para api http://127.0.0.1:8000
 #@ en fast api este decorador registra la funcion, cuando alguien llama a mi ruta raiz ejecuta la funcion que le sigue
-
 app.include_router(catalogo)
 
 
